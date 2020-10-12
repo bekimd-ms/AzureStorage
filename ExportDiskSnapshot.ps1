8097fb2a4ae559e1628881084fb46f7721352262
Bekim Demiroski <bekimd@microsoft.com>
2020-10-12 12:23:21 +0800

    Update ExportDiskSnapshot.ps1

--- ExportDiskSnapshot.ps1
+++ ExportDiskSnapshot.ps1
@@ -7,11 +7,13 @@ param(
 )
 
 $sas = Grant-AzureRmSnapshotAccess -ResourceGroupName $rg -SnapshotName $sn -DurationInSecond 3600 -Access Read
+$sas
 
-Write-Output $sas 
-$key = Get-AzureRmStorageAccountKey -ResourceGroupName $acctrg -Name $acct
-$ctx = New-AzureStorageContext -StorageAccountName $acct -StorageAccountKey $key[0].Value
+if( $acctrg -ne '' )
+{
+  $key = Get-AzureRmStorageAccountKey -ResourceGroupName $acctrg -Name $acct
+  $ctx = New-AzureStorageContext -StorageAccountName $acct -StorageAccountKey $key[0].Value
 
-#Copy the snapshot to the storage account 
-Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $cont  -DestContext $ctx -DestBlob $sn
-
+  #Copy the snapshot to the storage account 
+  Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $cont  -DestContext $ctx -DestBlob $sn
+}
